e9d44e32fa3382dda454c8638ad5a5cd16e8ab7e
Harpreet Singh <84502026+Harpreetsingh10414@users.noreply.github.com>
2021-08-10 16:09:15 +0530

    Calculate Simple interest

---
+++ 05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float r, f;
+    int i, t;
+    printf("Terms --\n--> Do not enter Initial amount in decimal\n--> Enter time in years");
+    printf("\nEnter annual interest rate - ");
+    scanf("%f", &r);
+    printf("\nEnter initial amount - ");
+    scanf("%d", &i);
+    printf("\nEnter time in years - ");
+    scanf("%d", &t);
+
+    f = i * (1 + (r * t));
+    printf("\nSimple Interest - %f", f);
+    return 0;
+}
